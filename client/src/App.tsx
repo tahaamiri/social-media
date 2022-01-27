d7b2bdfee5a63dc1566416f3867d50fee75467f3
amiriTaha <amiri.taha.98@gmail.com>
2022-01-27 12:55:08 +0330

    Tailwind add to project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className='text-red-400'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
       </header>
